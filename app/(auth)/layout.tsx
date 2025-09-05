b68c6fe3582fb5997d256c7ed1a7d7458a6c76e9
prabuddhaxdev <prabuddha1749@gmail.com>
2025-09-05 13:45:27 +0530

    deleted whole repo due to some errors not fixable for 2days and starting again

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="h-full flex items-center justify-center">{children}</div>
-  );
-}
-
-export default AuthLayout;
--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ServerId() {
-  return (
-    <div>ServerId</div>
-  )
-}
-
-export default ServerId
